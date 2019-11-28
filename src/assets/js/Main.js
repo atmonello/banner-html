8bdb071c09edf0df6f5f47d78e81e5fc34baabe3
Andre Monello <atmonello@gmail.com>
2019-11-27 23:55:14 -0300

    Implementando chamada assincrona do JSON

--- src/assets/js/Main.js
+++ src/assets/js/Main.js
@@ -1,39 +1,41 @@
 /* eslint-disable quote-props */
 import logoImage from '../img/voucher.png';
-import {
-  products,
-  logoUrl,
-} from './Data';
-
-import Product from './Product';
 import Cycles from './Cycles';
+import Product from './Product';
 
-const cycles = new Cycles();
-cycles.init({
-  total: products.length,
-  priceInterval: 2000,
-  productsInterval: 4000,
-});
+(async () => {
+  await fetch('http://localhost:3001/products')
+    .then((res) => res.json())
+    .then((data) => {
+      const { products, logo_url: logoUrl } = data;
+      const cycles = new Cycles();
+      cycles.init({
+        total: products.length,
+        priceInterval: 2000,
+        productsInterval: 4000,
+      });
 
-const logoElement = document.querySelector('.banner-wrapper__footer #logo');
-logoElement.src = logoImage;
+      const logoElement = document.querySelector('.banner-wrapper__footer #logo');
+      logoElement.src = logoImage;
 
-const logoLink = document.querySelector('.banner-wrapper__footer #link');
-logoLink.href = logoUrl;
-logoLink.setAttribute('target', '_blank');
+      const logoLink = document.querySelector('.banner-wrapper__footer #link');
+      logoLink.href = logoUrl;
+      logoLink.setAttribute('target', '_blank');
 
-const createProducts = (items) => {
-  const productsWrapper = document.querySelector('.banner-wrapper__products');
+      const createProducts = (items) => {
+        const productsWrapper = document.querySelector('.banner-wrapper__products');
 
-  items.forEach((item, index) => {
-    const newProduct = new Product();
-    newProduct.init({
-      product: item,
-      productID: index + 1,
-      wrapper: productsWrapper,
-      cycles,
+        items.forEach((item, index) => {
+          const newProduct = new Product();
+          newProduct.init({
+            product: item,
+            productID: index + 1,
+            wrapper: productsWrapper,
+            cycles,
+          });
+        });
+      };
+
+      createProducts(products);
     });
-  });
-};
-
-createProducts(products);
+})();
